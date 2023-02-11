a14098984813c6d77d97119859bfcc82a78aeb68
favourakpasi <akpasifavour@gmail.com>
2023-02-11 23:20:07 +0100

    set up

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput style={styles.textInput} defaultValue="You can type in me" />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  textInput: {
+    height: "auto",
+    borderColor: "gray",
+    borderRadius: 5,
+    borderWidth: 1,
+    padding: 10,
   },
 });
