0e3e52e1d6488da5e72d2e4e6c4bdc9f1fe04400
favourakpasi <akpasifavour@gmail.com>
2023-02-12 23:58:37 +0100

    refactor code

--- App.js
+++ App.js
@@ -1,43 +1,12 @@
 import React from "react";
-import {
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import SectionListBasics from "./components/ListView";
+import ScrollViewBasics from "./components/ScrollViewBasics";
 
 export default function App() {
   return (
-    <ScrollView style={styles.container}>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-      </View>
-      <TextInput style={styles.textInput} defaultValue="You can type in me" />
-    </ScrollView>
+    <>
+      <ScrollViewBasics />
+      <SectionListBasics />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    paddingTop: 50,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  textInput: {
-    height: "auto",
-    borderColor: "gray",
-    borderRadius: 5,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
---
+++ components/ScrollViewBasics.jsx
@@ -0,0 +1,44 @@
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+const ScrollViewBasics = () => {
+  return (
+    <ScrollView style={styles.container}>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput style={styles.textInput} defaultValue="You can type in me" />
+    </ScrollView>
+  );
+};
+
+export default ScrollViewBasics;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  textInput: {
+    height: "auto",
+    borderColor: "gray",
+    borderRadius: 5,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
